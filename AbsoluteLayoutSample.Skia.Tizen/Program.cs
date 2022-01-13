f14fb0689afde3a55708f9841311a7993590ea97
Pallavi Chaudhari <pallavi.chaudhari@aircanada.ca>
2022-01-13 14:27:49 -0500

    Add project files.

---
+++ AbsoluteLayoutSample.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace AbsoluteLayoutSample.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new AbsoluteLayoutSample.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ AbsoluteLayoutSample.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace AbsoluteLayoutSample.Skia.Gtk
+{
+}
